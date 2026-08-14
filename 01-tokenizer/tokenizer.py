cd465402fb88e1c83f248986cbac4e317753239d
Ahmadhaiwala <haiwalaahmad7@gmail.com>
2026-08-14 17:43:25 +0530

    basic tokenizer success

---
+++ 01-tokenizer/tokenizer.py
@@ -0,0 +1,42 @@
+class Tokenizer:
+    def __init__(self):
+        self.vocab = {}
+        self.inverse_vocab = {}
+
+    def train(self, text):
+        counter = 0
+
+        for char in text:
+            if char not in self.vocab:
+                self.vocab[char] = counter
+                counter += 1
+
+        for char in self.vocab:
+            self.inverse_vocab[self.vocab[char]] = char
+
+    def encode(self, text):
+        ids=[]
+        for i in text:
+            ids.append(self.vocab[i])
+        return ids
+
+
+
+    def decode(self, ids):
+        chars = []
+
+        for id in ids:
+            chars.append(self.inverse_vocab[id])
+
+        return "".join(chars)
+    
+
+#testing
+tokenizer=Tokenizer()
+tokenizer.train("hello world")
+print(tokenizer.vocab)
+print(tokenizer.inverse_vocab)
+encoded = tokenizer.encode("hello world")
+
+print(encoded)
+print(tokenizer.decode(encoded))
